15ccfe54361934769648c265c5d7ace9710a54c2
Ricardo-Alexandre <rr147519@gmail.com>
2023-04-19 18:55:46 -0300

    dia 5.2

---
+++ fundamentos/secao-3/dia-1/script.js
@@ -0,0 +1,17 @@
+let status = 'aprovada' ;
+status = 'lista' ;
+status = 'reprovada' ;
+
+switch(status) {
+    case 'aprovada':
+      console.log("Parabéns, você está no grupo de pessoas aprovadas!");
+      break;
+    case 'lista':
+      console.log("Você está na nossa lista de espera.");
+      break;
+    case 'reprovada':
+      console.log("Infelizmente, você reprovou.");
+      break;
+    default:
+      console.log("Informação incorreta.");
+  }
